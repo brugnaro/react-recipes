9c863ec93ee772393ebb12805cefa53956b54e1e
marcos <marcosrodriguesbrugnaro@gmail.com>
2019-02-07 23:28:39 -0200

    Upload project

---
+++ adm/.neutrinorc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  use: [
+    '@neutrinojs/standardjs',
+    [
+      '@neutrinojs/react',
+      {
+        html: {
+          title: 'adm'
+        }
+      }
+    ],
+    '@neutrinojs/jest'
+  ]
+};

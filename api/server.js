febe0ee4cb16f71e1aaa5bcc5f6010ff00ca8422
marcos <marcosrodriguesbrugnaro@gmail.com>
2019-02-12 21:01:24 -0200

    First mutation

--- api/server.js
+++ api/server.js
@@ -1,12 +1,44 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 require('dotenv').config({ path: 'variables.env' })
 
+const Recipe = require('./models/Recipe')
+const User = require('./models/User')
+
+// Bring in graphql-express middleware
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express')
+const { makeExecutableSchema } = require('graphql-tools')
+
+const { typeDefs } = require('./schema')
+const { resolvers } = require('./resolvers')
+
+// Create schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+// Connect to database
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('DB connected'))
   .catch(err => console.error(err))
 
+// Initialize application
 const app = express()
+
+// Create graphiql application
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+
+// Connect schemas with graphql
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema,
+  context: {
+    Recipe,
+    User
+  }
+}))
+
 const PORT = process.env.PORT || 4444
 app.listen(PORT, () => { console.log(`Server listening on PORT ${PORT}`) })
